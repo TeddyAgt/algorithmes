fc32d1b8bc68a02b7b2d813b9a0fb1f5ad23d398
Teddy Aboim <teddy.aboim@gmail.com>
2024-04-13 08:03:19 +0200

    add 3 new versions of bubble sort

--- 03 - [tableaux - listes] - Tris simples/bubble_sort.mjs
+++ 03 - [tableaux - listes] - Tris simples/bubble_sort.mjs
@@ -16,4 +16,63 @@ const arr = [50, 22, 1, 23, 12];
 //   return arr;
 // }
 
+// V2 (optimisé)
+
+// function bubbleSort(arr) {
+//   let right = arr.length; // A chaque itération, on aura un élément trié de plus sur la droite du tableau
+
+//   while (right > 0) {
+//     let swap = false;
+//     for (let i = 1; i < right; i++) {
+//       if (arr[i] < arr[i - 1]) {
+//         const tmp = arr[i - 1];
+//         arr[i - 1] = arr[i];
+//         arr[i] = tmp;
+//         swap = true;
+//       }
+//     }
+//     if (!swap) break; // S'il n'y a eu aucun swap lors de l'itération c'est que le tableau est déjà trié
+//     right--;
+//   }
+//   return arr;
+// }
+
+// V3 (optimisé)
+
+// function bubbleSort(arr) {
+//   let right = arr.length;
+//   let swap;
+//   do {
+//     for (let i = 1; i < right; i++) {
+//       swap = false;
+//       if (arr[i] < arr[i - 1]) {
+//         const tmp = arr[i - 1];
+//         arr[i - 1] = arr[i];
+//         arr[i] = tmp;
+//         swap = true;
+//       }
+//     }
+//     right--;
+//   } while (swap);
+//   return arr;
+// }
+
+// V4 (optimisé)
+
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let swap = false;
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+        swap = true;
+      }
+    }
+    if (!swap) break;
+  }
+  return arr;
+}
+
 console.log(bubbleSort(arr));
