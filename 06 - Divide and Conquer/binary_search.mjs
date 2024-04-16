ff72338733c66cbbdf3fd32142f056c73ab5898a
Teddy Aboim <teddy.aboim@gmail.com>
2024-04-16 07:37:36 +0200

    add binary search by divide and conquer

---
+++ 06 - Divide and Conquer/binary_search.mjs
@@ -0,0 +1,18 @@
+const array = new Array(100).fill().map((x, i) => i + 1);
+const numberToGuess = Math.trunc(Math.random() * 100);
+
+function binarySearch(array, numberToGuess, start, end) {
+  if (start > end) return null;
+  const mid = Math.floor((start + end) / 2);
+  if (array[mid] === numberToGuess) {
+    return mid;
+  } else if (array[mid] > numberToGuess) {
+    return binarySearch(array, numberToGuess, start, mid - 1);
+  } else {
+    return binarySearch(array, numberToGuess, mid + 1, end);
+  }
+}
+
+const result = binarySearch(array, numberToGuess, 0, array.length - 1);
+console.log(`Number to guess: ${numberToGuess}`);
+console.log(`Index: ${result}`);
