ce2e31dce3b5ea783a240aa2c1074e3abc602e06
TeddyAgt <teddy.aboim@gmail.com>
2024-06-06 18:22:42 +0200

    add dfs on graphs

---
+++ 15 - Les graphes/dfs_graph.mjs
@@ -0,0 +1,24 @@
+import { Stack } from "../08 - Stacks/stack_array.mjs";
+import { Graph } from "./graph.mjs";
+
+function deepFirstSearch(graph, vertex) {
+  const visited = [];
+  const results = {};
+  const stack = new Stack();
+
+  stack.push(vertex);
+
+  while (!stack.isEmpty()) {
+    const currentVertex = stack.pop();
+    if (!visited[currentVertex]) {
+      visited[currentVertex] = true;
+      results.push(currentVertex);
+
+      for (let i = 0; i < graph.getVertex(currentVertex).length; i++) {
+        if (!visited[graph.getVertex(currentVertex)[i]]) {
+          stack.push(graph.getVertex(currentVertex)[i]);
+        }
+      }
+    }
+  }
+}
